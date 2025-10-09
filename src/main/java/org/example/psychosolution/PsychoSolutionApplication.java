74c6b9e01e4c57b6b66e1ace0a91bf285b60fc52
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-08 23:54:21 -0300

    Inicio do Projeto

---
+++ src/main/java/org/example/psychosolution/PsychoSolutionApplication.java
@@ -0,0 +1,13 @@
+package org.example.psychosolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychoSolutionApplication.class, args);
+    }
+
+}
