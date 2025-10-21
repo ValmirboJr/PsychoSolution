027a7d420a8c3ca4be4bf6ddb60f2acdfb761bbf
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-20 21:42:16 -0300

    Feat:Criando a classe que solicita os dados

--- src/main/java/org/example/psychosolution/request/PacientesRequest.java
+++ src/main/java/org/example/psychosolution/request/PacientesRequest.java
@@ -1,4 +1,18 @@
 package org.example.psychosolution.request;
 
-public class PacientesRequest {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+@Builder
+public record PacientesRequest(String nome,
+                               String cpf,
+                               int idade,
+                               String telefone,
+                               String email,
+                               String senha,
+                               @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+                               LocalDate dataNascimento
+                               ) {
 }
