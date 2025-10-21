45104ceba46397d435e59c3435e4fd64ef3fb596
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-20 21:45:46 -0300

    Feat:Criando a classe que é responsavel pelos endpoints da aplicação

--- src/main/java/org/example/psychosolution/controller/PacientesController.java
+++ src/main/java/org/example/psychosolution/controller/PacientesController.java
@@ -1,4 +1,44 @@
 package org.example.psychosolution.controller;
 
+import org.example.psychosolution.entity.Pacientes;
+import org.example.psychosolution.mapper.PacientesMapper;
+import org.example.psychosolution.request.PacientesRequest;
+import org.example.psychosolution.response.PacientesResponse;
+import org.example.psychosolution.service.PacientesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/pacientes")
 public class PacientesController {
+
+    @Autowired
+    private PacientesService pacientesService;
+
+    @PostMapping("/save")
+    public ResponseEntity<PacientesResponse>salvar(@RequestBody PacientesRequest request){
+        Pacientes save = pacientesService.Registro(PacientesMapper.toPacientes(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(PacientesMapper.toPacientesResponse(save));
+    }
+    @GetMapping()
+    public ResponseEntity<List<PacientesResponse>>listarPaciente(){
+        return ResponseEntity.ok(pacientesService.ListarPacientes()
+                .stream().map(PacientesMapper::toPacientesResponse).toList());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PacientesResponse>listarPacientesPorID(@PathVariable UUID id){
+        return pacientesService.BuscarPaciente(id)
+                .map(pacientes -> ResponseEntity.ok(PacientesMapper.toPacientesResponse(pacientes)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @DeleteMapping("/deletar")
+    public ResponseEntity<PacientesResponse>deletarPaciente(UUID id){
+        pacientesService.apagarCadastro(id);
+        return  ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
