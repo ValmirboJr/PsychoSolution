4de836fe0a8eab11ba6affac8ec089cb5224847f
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-20 21:43:48 -0300

    Feat:Criando a classe que é responsavel pelo tratamento dos dados

--- src/main/java/org/example/psychosolution/response/PacientesResponse.java
+++ src/main/java/org/example/psychosolution/response/PacientesResponse.java
@@ -1,4 +1,19 @@
 package org.example.psychosolution.response;
 
-public class PacientesResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Builder
+public record PacientesResponse(UUID id,
+                                String nome,
+                                String cpf,
+                                int idade,
+                                String telefone,
+                                String email,
+                                @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+                                LocalDate dataNascimento
+                                ) {
 }
