68c0e2e5232c2a749ecc9f1be2be7bb62b88335d
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-20 21:37:06 -0300

    Fix:Ajustando a classe que conversa com o banco

--- src/main/java/org/example/psychosolution/repository/ConsultaRepository.java
+++ src/main/java/org/example/psychosolution/repository/ConsultaRepository.java
@@ -1,7 +1,8 @@
-package repository;
+package org.example.psychosolution.repository;
 
+import org.example.psychosolution.entity.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
-import model.Consulta;
+
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 }
