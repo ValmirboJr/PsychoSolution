806e2112c998767881728d0e42a40aefcb46c61e
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-23 23:59:22 -0300

    Feat:Criando metodo para retorno das Consultas do paciente

--- src/main/java/org/example/psychosolution/service/PacientesService.java
+++ src/main/java/org/example/psychosolution/service/PacientesService.java
@@ -1,10 +1,13 @@
 package org.example.psychosolution.service;
 
+import org.example.psychosolution.entity.Consulta;
 import org.example.psychosolution.entity.Pacientes;
+import org.example.psychosolution.repository.ConsultaRepository;
 import org.example.psychosolution.repository.PacientesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,22 +18,26 @@ public class PacientesService {
     @Autowired
     private PacientesRepository pacientesRepository;
 
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
     public Pacientes Registro(Pacientes pacientes) {
         if (pacientesRepository.findByEmail(pacientes.getEmail()).isPresent()) {
-        throw new RuntimeException("Paciente ja existe");
+            throw new RuntimeException("Paciente ja existe");
         }
         //Adicionar Depois Encriptador de senha
         return pacientesRepository.save(pacientes);
     }
-    public List<Pacientes> ListarPacientes(){
+
+    public List<Pacientes> ListarPacientes() {
         return pacientesRepository.findAll();
     }
 
-    public Optional<Pacientes> BuscarPaciente(UUID idunico){
+    public Optional<Pacientes> BuscarPaciente(UUID idunico) {
         return pacientesRepository.findById(idunico);
     }
 
-    public Optional<Pacientes> AlterarCadastro(Pacientes pacientes, UUID idunico){
+    public Optional<Pacientes> AlterarCadastro(UUID idunico, Pacientes pacientes) {
         Optional<Pacientes> OptPacientes = pacientesRepository.findById(idunico);
         if (OptPacientes.isPresent()) {
 
@@ -39,13 +46,23 @@ public class PacientesService {
             paciente.setIdade(pacientes.getIdade());
             paciente.setNome(pacientes.getNome());
             paciente.setTelefone(pacientes.getTelefone());
+            paciente.setCpf(pacientes.getCpf());
+            paciente.setDataNascimento(pacientes.getDataNascimento());
             paciente.setSenha(pacientes.getSenha());
             pacientesRepository.save(paciente);
             return Optional.of(paciente);
         }
         return Optional.empty();
     }
-    public void apagarCadastro(UUID idunico){
+
+    public void apagarCadastro(UUID idunico) {
         pacientesRepository.deleteById(idunico);
+    }
+
+    public List<Consulta> consultasPaciente(UUID idPaciente) {
+        if (pacientesRepository.findById(idPaciente).isEmpty()) {
+            throw new RuntimeException("Paciente não encontrado");
+        }
+        return consultaRepository.findByPacienteId(idPaciente);
     }
 }
