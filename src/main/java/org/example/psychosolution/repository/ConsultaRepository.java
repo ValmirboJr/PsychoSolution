63c32f11759c3bfa1320c675aa8ef58bc6eff408
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-24 00:03:52 -0300

    Feat:Criando Metodo de listagem de Consulta para o paciente

--- src/main/java/org/example/psychosolution/repository/ConsultaRepository.java
+++ src/main/java/org/example/psychosolution/repository/ConsultaRepository.java
@@ -3,6 +3,11 @@ package org.example.psychosolution.repository;
 import org.example.psychosolution.entity.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+    List<Consulta> findByPacienteId(UUID idPaciente);
 }
