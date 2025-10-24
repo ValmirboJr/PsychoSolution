b5fd487ef97f1b3c32145c716ebf4fed4113686f
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-24 00:06:34 -0300

    Feat:Criando Classe responsavel pelo mapeamento dos dados

--- src/main/java/org/example/psychosolution/mapper/ConsultaMapper.java
+++ src/main/java/org/example/psychosolution/mapper/ConsultaMapper.java
@@ -1,4 +1,27 @@
 package org.example.psychosolution.mapper;
 
+import lombok.experimental.UtilityClass;
+import org.example.psychosolution.entity.Consulta;
+import org.example.psychosolution.request.ConsultaRequest;
+import org.example.psychosolution.response.ConsultaResponse;
+
+@UtilityClass
 public class ConsultaMapper {
+    public static Consulta toConsulta(ConsultaRequest consultaRequest){
+        return Consulta
+                .builder()
+                .paciente(consultaRequest.paciente())
+                .data(consultaRequest.data())
+                .status(consultaRequest.status())
+                .build();
+    }
+    public static ConsultaResponse toConsultaResponse(Consulta consulta){
+        return ConsultaResponse
+                .builder()
+                .id(consulta.getId())
+                .paciente(consulta.getPaciente())
+                .status(consulta.getStatus())
+                .data(consulta.getData())
+                .build();
+    }
 }
