c2f0dc031c4c4872a8cb8b525ae32c1ccedc5bc4
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-24 08:04:17 -0300

    feat:Adicionando login e encriptador de senha

--- src/main/java/org/example/psychosolution/service/PsicologosService.java
+++ src/main/java/org/example/psychosolution/service/PsicologosService.java
@@ -1,4 +1,25 @@
 package org.example.psychosolution.service;
 
+import org.example.psychosolution.entity.Psicologos;
+import org.example.psychosolution.repository.PsicologosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class PsicologosService {
+
+    @Autowired
+    private PsicologosRepository psicologosRepository;
+
+    public Psicologos findPsicologosById(UUID id) {
+        return psicologosRepository.findById(id).orElse(null);
+    }
+    public Psicologos salvar(Psicologos psicologos) {
+        if (psicologosRepository.findByCrp(psicologos.getCrp()).isPresent()) {
+            throw new RuntimeException("CRP já Cadastrado");
+        }
+        return psicologosRepository.save(psicologos);
+    }
 }
