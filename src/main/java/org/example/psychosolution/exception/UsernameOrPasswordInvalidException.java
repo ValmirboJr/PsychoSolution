e9b5e92146e20610959f86b835a45faa01716e5f
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-24 08:19:13 -0300

    feat:Criando exceção para validar o usuario e senha

--- src/main/java/org/example/psychosolution/exception/UsernameOrPasswordInvalidException.java
+++ src/main/java/org/example/psychosolution/exception/UsernameOrPasswordInvalidException.java
@@ -1,7 +1,7 @@
 package org.example.psychosolution.exception;
 
 public class UsernameOrPasswordInvalidException extends RuntimeException {
-  public UsernameOrPasswordInvalidException(String message) {
-    super(message);
-  }
+    public UsernameOrPasswordInvalidException(String message) {
+        super(message);
+    }
 }
