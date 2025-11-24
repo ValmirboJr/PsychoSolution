9582472d90830562a6ccde92bb6e821d05c715ae
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-24 08:14:05 -0300

    feat:Classe criada para suportar os erros de usuario e senha invalidos

--- src/main/java/org/example/psychosolution/config/AplicationControllerAdvice.java
+++ src/main/java/org/example/psychosolution/config/AplicationControllerAdvice.java
@@ -1,4 +1,31 @@
 package org.example.psychosolution.config;
 
+import org.example.psychosolution.exception.UsernameOrPasswordInvalidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class AplicationControllerAdvice {
+
+    @ExceptionHandler(UsernameOrPasswordInvalidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleNotFoundException (UsernameOrPasswordInvalidException ex) {
+        return ex.getMessage();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgsNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            errors.put(((FieldError)error).getField(), error.getDefaultMessage());
+        });
+        return errors;
+    }
 }
