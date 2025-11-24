f5f12a93189c7d6ebd7ddde8e50ea13dc1152cae
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-24 08:09:25 -0300

    fix:Ajustando busca com o UUID

--- src/main/java/org/example/psychosolution/repository/PsicologosRepository.java
+++ src/main/java/org/example/psychosolution/repository/PsicologosRepository.java
@@ -1,4 +1,13 @@
 package org.example.psychosolution.repository;
 
-public interface PsicologosRepository {
+import org.example.psychosolution.entity.Psicologos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PsicologosRepository extends JpaRepository<Psicologos, UUID> {
+
+    Optional<Psicologos> findByCrp(String crp);
+
 }
